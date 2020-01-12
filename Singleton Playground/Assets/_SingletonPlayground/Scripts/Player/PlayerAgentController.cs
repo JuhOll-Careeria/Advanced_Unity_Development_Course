47e0d0d025f39261457d145ef63ffd6513abb96c
Olli Juhola <juhola.olli@gmail.com>
2020-01-12 18:55:09 +0200

    Started working on Playground scene, implemented simple moving & camera

---
+++ Singleton Playground/Assets/_SingletonPlayground/Scripts/Player/PlayerAgentController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerAgentController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    NavMeshAgent agent;
+    Rigidbody rb;
+    Animator anim;
+
+    Vector2 movementDir = Vector2.zero;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        agent.speed = this.speed;
+    }
+
+    private void FixedUpdate()
+    {
+        SimpleAgentMove();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    void SimpleAgentMove()
+    {
+        movementDir.x = Input.GetAxis("Horizontal");
+        movementDir.y = Input.GetAxis("Vertical");
+
+        if (movementDir.magnitude > 0f && agent.isActiveAndEnabled)
+        {
+            agent.SetDestination(transform.localPosition + new Vector3(movementDir.x, 0, movementDir.y));
+        }
+
+        GetComponentInChildren<Animator>().SetFloat("velocity", agent.velocity.magnitude);
+    }
+
+    void Attack()
+    {
+        // Toteuta Attack animaatio, layerillä 1
+        anim.Play("Attack", 1, 0);
+    }
+}
