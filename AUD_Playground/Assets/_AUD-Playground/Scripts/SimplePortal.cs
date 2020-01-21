1d0666053557dea4c0f858c57e2e5d95910cbf5d
Olli Juhola <juhola.olli@gmail.com>
2020-01-21 18:43:12 +0200

    Small tweak

--- AUD_Playground/Assets/_AUD-Playground/Scripts/SimplePortal.cs
+++ AUD_Playground/Assets/_AUD-Playground/Scripts/SimplePortal.cs
@@ -5,29 +5,15 @@ using UnityEngine;
 public class SimplePortal : MonoBehaviour
 {
     [SerializeField] private List<string> TriggerTags = new List<string>();
-
     [SerializeField] private SoundEffect PortalEnterSound;
-
-    // Testausta varten oleva variable
-    public bool UseSceneReference = false;
-
     [SerializeField] private string LevelName;
-    [SerializeField] private SceneReference Level;
 
     public void OnTriggerEnter(Collider other)
     {
         if (TriggerTags.Contains(other.gameObject.tag))
         {
-            // Testausta varten tehty tarkistus, kattoo jos UseSceneReference on true tai false.
-            if (!UseSceneReference)
-            {
-                // Lataa uusi leveli stringin perusteella
-                LevelManager.Instance.LoadLevel(LevelName);
-            } else
-            {
-                // lataa uusi leveli SceneReference perusteella
-                LevelManager.Instance.LoadLevel(Level);
-            }
+            // Lataa uusi leveli stringin perusteella
+            LevelManager.Instance.LoadLevel(LevelName);
 
             AudioManager.Instance.PlayClipOnce(PortalEnterSound);
         }
